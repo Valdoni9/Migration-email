912c5953bf68f96f4c389f8062f117964f645da0
Valdoni <30943280+Valdoni9@users.noreply.github.com>
2024-09-18 22:55:30 -0400

    atualização README

--- migracao-emails.ps1
+++ migracao-emails.ps1
@@ -3,47 +3,12 @@ $imapsyncPath = "C:\Users\valdo\Downloads\Sites migração\imapsync_2.290\imapsy
 
 # Lista de e-mails a serem migrados
 $emails = @(
-    "contato@arloesitech.com.br",
-    "eder.reverdito@arloesitech.com.br",
-    "diretoria@arloesitech.com.br",
-    "monitoramento@arloesitech.com.br",
-    "dp@arloesitech.com.br",
-    "daniela.ramos@arloesitech.com.br",
-    "rebecca.bueno@arloesitech.com.br",
-    "amauri.junior@arloesitech.com.br",
-    "vault@arloesitech.com.br",
-    "dev@arloesitech.com.br",
-    "chavepix@arloesitech.com.br",
-    "financeiro@arloesitech.com.br",
-    "wilcson.denner@arloesitech.com.br",
-    "comercial@arloesitech.com.br",
-    "anny.dias@arloesitech.com.br",
-    "suporte@arloesitech.com.br",
-    "caio.castro@arloesitech.com.br",
-    "engenharia@arloesitech.com.br",
-    "administrativo@arloesitech.com.br",
-    "igor.brandao@arloesitech.com.br",
-    "dss@arloesitech.com.br",
-    "recrutamento@arloesitech.com.br",
-    "fernando.bellezzia@arloesitech.com.br",
-    "git@arloesitech.com.br",
-    "maria.fernanda@arloesitech.com.br",
-    "desenvolvedor@arloesitech.com.br",
-    "vitor.lima@arloesitech.com.br",
-    "pix@arloesitech.com.br",
-    "samira.amorim@arloesitech.com.br",
-    "danielle.camilo@arloesitech.com.br",
-    "vinicius.eugenio@arloesitech.com.br",
-    "monica.ribeiro@arloesitech.com.br",
-    "licitacoes@arloesitech.com.br",
-    "airton.silva@arloesitech.com.br",
-    "no-reply@arloesitech.com.br",
-    "guilherme.alexandre@arloesitech.com.br",
-    "compras@arloesitech.com.br"
+    "contato@example.com.br",
+    "contato.example@example.com.br"
 )
 
 # Senha única para todos os e-mails
-$senha = "Arlo@124353"
+$senha = "12345@"
 
 # Informações do servidor de origem
 $servidorOrigem = "imap.uni5.net"
